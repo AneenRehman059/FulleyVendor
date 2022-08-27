eb768086c72a3058a87db11012106c8b0aab8a61
hafiz <aneeshafiz516@gmail.com>
2022-08-27 01:02:26 -0700

    proj comp

--- app/src/main/java/com/zasa/fuellyvendor/SettingsFragment.java
+++ app/src/main/java/com/zasa/fuellyvendor/SettingsFragment.java
@@ -16,7 +16,7 @@ public class SettingsFragment extends Fragment {
 
 
     LinearLayout change_tpin;
-    Button shareBtn;
+    Button shareBtn,viewProf;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -28,28 +28,39 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-      change_tpin = view.findViewById(R.id.edit_tpin_layout);
-      shareBtn = view.findViewById(R.id.share_btn);
 
-      change_tpin.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              Intent intent = new Intent(getActivity().getApplication(),Change_Tpin_Activity.class);
-              startActivity(intent);
-          }
-      });
+        viewProf = view.findViewById(R.id.btn_view_prof);
 
-      shareBtn.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              String shareBody = "Share app via";
-              Intent intent = new Intent();
-              intent.setAction(Intent.ACTION_SEND);
-              intent.setType("text/plain");
-              intent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-              startActivity(Intent.createChooser(intent,"Share App"));
-          }
-      });
+        viewProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplication(),ViewProfile.class);
+                startActivity(intent);
+            }
+        });
+
+//      change_tpin = view.findViewById(R.id.edit_tpin_layout);
+//      shareBtn = view.findViewById(R.id.share_btn);
+
+//      change_tpin.setOnClickListener(new View.OnClickListener() {
+//          @Override
+//          public void onClick(View v) {
+//              Intent intent = new Intent(getActivity().getApplication(),Change_Tpin_Activity.class);
+//              startActivity(intent);
+//          }
+//      });
+
+//      shareBtn.setOnClickListener(new View.OnClickListener() {
+//          @Override
+//          public void onClick(View v) {
+//              String shareBody = "Share app via";
+//              Intent intent = new Intent();
+//              intent.setAction(Intent.ACTION_SEND);
+//              intent.setType("text/plain");
+//              intent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+//              startActivity(Intent.createChooser(intent,"Share App"));
+//          }
+//      });
 
         return view;
     }
